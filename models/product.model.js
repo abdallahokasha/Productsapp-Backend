68913f4972bf949d8a832856c1ec1699e1b56b33
abdallahokasha <okasha.fci@gmail.com>
2019-01-04 23:54:26 +0200

    initial commit -remote database added on mLab -products controller added -products model added

---
+++ models/product.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let ProductSchema = new Schema({
+    name: {type: String, required: true, max: 100},
+    price: {type: Number, required: true},
+    brand: {type: String, required: true, max: 100},
+    image: {type: String, required: true},
+    category: [{ id: Number, name: String }],
+});
+
+
+// Export the model
+module.exports = mongoose.model('Product', ProductSchema);
