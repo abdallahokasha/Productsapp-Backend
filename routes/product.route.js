975ad40075dfc684403c614ca9611b078fb66ec6
abdallahokasha <okasha.fci@gmail.com>
2019-01-06 21:15:59 +0200

    -cors added to route

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
-
 const product_controller = require('../controllers/product.controller');
 
 // a simple test url to check that all of our files are communicating correctly.
+
+// router.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
+router.use(cors());
+
 router.get('/list', product_controller.product_list);
 router.get('/:id', product_controller.product_details);
 router.get('/*', product_controller.product_search);
