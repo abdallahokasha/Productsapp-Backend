2527f453eaf430018e6ef70dcdad829d8385e0d6
abdallahokasha <okasha.fci@gmail.com>
2019-01-07 06:11:47 +0200

    -reset cors again to be opened

--- routes/product.route.js
+++ routes/product.route.js
@@ -4,21 +4,21 @@ const router = express.Router();
 const product_controller = require('../controllers/product.controller');
 
 
-//router.use(cors());
+router.use(cors());
 
-// Set up a whitelist and check against it
-var whitelist = ['https://products-fb1dc.firebaseapp.com', 'http://localhost:3000']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-// Then pass them to cors
-router.use(cors(corsOptions));
+// // Set up a whitelist and check against it
+// var whitelist = ['https://products-fb1dc.firebaseapp.com', 'http://localhost:3000']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// // Then pass them to cors
+// router.use(cors(corsOptions));
 
 router.get('/list', product_controller.product_list);
 router.get('/:id', product_controller.product_details);
